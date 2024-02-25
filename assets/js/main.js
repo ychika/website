fac3bfb6b27a4f918324a1e7f24e781de0f51e04
chika <7546631+ychika@users.noreply.github.com>
2024-02-25 22:01:42 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,6 +254,31 @@
     })
   });
 
+
+  /* set image height for iOS browser */
+  
+  const setFillHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  
+  let vw = window.innerWidth;
+  
+  window.addEventListener('resize', () => {
+    if (vw === window.innerWidth) {
+    　// 画面の横幅にサイズ変動がないので処理を終える
+      return;
+    }
+  
+    // 画面の横幅のサイズ変動があった時のみ高さを再計算する
+    vw = window.innerWidth;
+    setFillHeight();
+  });
+  
+  // 初期化
+  setFillHeight();
+
+
   /**
    * Initiate Pure Counter 
    */
